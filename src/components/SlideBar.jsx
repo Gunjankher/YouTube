3786a25451fd8cbca930d1c294e2745bfada04c4
Gunjankher <gkhercode@gmail.com>
2024-05-24 09:32:30 +0530

    sidebar

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React from 'react';
+import {
+    MdHomeFilled,
+    MdOutlineSlowMotionVideo,
+    MdSubscriptions,
+    MdOutlineVideoLibrary,
+    MdHistory,
+    MdOutlineWatchLater,
+} from 'react-icons/md';
 
-function SlideBar() {
-  return (
-    <div className='text-white bg-[#222222] w-[300px] h-screen'>
-        
-        
-        
-        
-        SlideBar</div>
-  )
+import { LuThumbsUp } from 'react-icons/lu';
+
+export default function SideBar() {
+    const mainLinks = [
+        {
+            icon: <MdHomeFilled className='text-xl' />,
+            name: 'Home'
+        },
+        {
+            icon: <MdOutlineSlowMotionVideo className='text-xl' />,
+            name: 'Shorts'
+        },
+        {
+            icon: <MdSubscriptions className='text-xl' />,
+            name: 'Subscriptions'
+        }
+    ];
+
+    const otherLinks = [
+        {
+            icon: <MdOutlineVideoLibrary className='text-xl' />,
+            name: 'Library'
+        },
+        {
+            icon: <MdHistory className='text-xl' />,
+            name: 'History'
+        },
+        {
+            icon: <MdOutlineWatchLater className='text-xl' />,
+            name: 'Watch Later'
+        },
+        {
+            icon: <LuThumbsUp className='text-xl' />,
+            name: 'Liked Video'
+        }
+    ];
+
+    return (
+      <></>
+    )
 }
-
-export default SlideBar
